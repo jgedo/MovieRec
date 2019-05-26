712fd90352b9712b8e4e671f844580248067b248
Javier Gomez Edo <jge1591@gmail.com>
2019-05-26 17:41:02 -0500

    first commit

---
+++ src/main/java/com/movierec/MovieRec/MovieRecApplication.java
@@ -0,0 +1,14 @@
+package com.movierec.MovieRec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecApplication.class, args);
+		System.out.println();
+	}
+
+}
